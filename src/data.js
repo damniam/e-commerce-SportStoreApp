9fb80b2f026487f3a947ad4cb613bc239d9a7d05
Damian Dzialo <ddzialo28@interia.pl>
2021-01-20 17:36:48 +0100

    Config server

---
+++ src/data.js
@@ -0,0 +1,71 @@
+module.exports = function () {
+  return {
+    categories: ["Watersports", "Football", "Chess"],
+    product: [
+      {
+        id: 1,
+        name: "Kayak",
+        category: "Watersports",
+        description: "A boat for one person",
+        price: 245,
+      },
+      {
+        id: 2,
+        name: "Lifejacket",
+        category: "Watersports",
+        description: "Protective and fashionable",
+        price: 46.5,
+      },
+      {
+        id: 3,
+        name: "Football ball",
+        category: "Football",
+        description: "A football ball",
+        price: 19.5,
+      },
+      {
+        id: 4,
+        name: "Corner Flags",
+        category: "Football",
+        description: "Give your playing field a professional touch",
+        price: 35,
+      },
+      {
+        id: 5,
+        name: "Stadium",
+        category: "Football",
+        description: "35,000-seat stadium",
+        price: 890525,
+      },
+      {
+        id: 6,
+        name: "Thinking cap",
+        category: "Chess",
+        description: "Improve brain efficiency by 75%",
+        price: 16,
+      },
+      {
+        id: 7,
+        name: "Unsteady chair",
+        category: "Chess",
+        description: "Secretly give your opponent a disadvantage",
+        price: 30,
+      },
+      {
+        id: 7,
+        name: "Chess board",
+        category: "Chess",
+        description: "Wooden chess board",
+        price: 95,
+      },
+      {
+        id: 7,
+        name: "Bling bling King",
+        category: "Chess",
+        description: "Gold-plated, diamond-studded king",
+        price: 1295,
+      },
+    ],
+    orders: [],
+  };
+};
